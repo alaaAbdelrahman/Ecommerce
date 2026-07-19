8c5545723c099368f630c6b73a5b8aa5627ad5f6
IT <a.abdelrahman1399@gmail.com>
2026-07-19 14:23:16 +0300

    clean prohject

---
+++ controller/Category.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Ecommerce.controller
+{
+    [Route("[controller]")]
+    public class Category : Controller
+    {
+        private readonly ILogger<Category> _logger;
+
+        public Category(ILogger<Category> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
